693cc56e0670d0eee2158771ecd595d7f16b57ac
Koios <koios1016@gmail.com>
2018-09-04 20:42:26 +0800

    类视图版本

--- polls/urls.py
+++ polls/urls.py
@@ -6,12 +6,19 @@ from . import views
 app_name = "polls"
 urlpatterns = [
     # path('', views.index),
-    # ex: /polls/
-    re_path(r'^$', views.index, name='index'),
-    # ex: /polls/5/
-    re_path(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    re_path(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    # ex: /polls/5/vote/
+    # # ex: /polls/
+    # re_path(r'^$', views.index, name='index'),
+    # # ex: /polls/5/
+    # re_path(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # # ex: /polls/5/results/
+    # re_path(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # # ex: /polls/5/vote/
+    # re_path(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+
+    # 改用类视图
+    # 第2,3条目中将原来的<question_id>修改成了<pk>
+    re_path(r'^$', views.IndexView.as_view(), name='index'),
+    re_path(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    re_path(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     re_path(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -2,10 +2,11 @@ from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views import generic
 
 from polls.models import Question, Choice
 
-
+'''
 def index(request):
     # return HttpResponse("Hello World.You're at the polls index.")
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -38,7 +39,7 @@ def results(request, question_id):
     # return HttpResponse(response % question_id)
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
-
+'''
 
 def vote(request, question_id):
     # return HttpResponse("You're voting on question %s." % question_id)
@@ -55,4 +56,26 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         # 成功处理数据后，自动跳转到结果页面，防止用户连续多次提交。
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,))) 
+
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        '''返回最近发布的5个问卷.'''
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
