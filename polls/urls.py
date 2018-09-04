6b217f142d51e21c106f223a8bee341e7537da93
Koios <koios1016@gmail.com>
2018-09-04 20:19:52 +0800

    初始版本

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,17 @@
 from django.conf.urls import url
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
+app_name = "polls"
 urlpatterns = [
-    path('', views.index)
+    # path('', views.index),
+    # ex: /polls/
+    re_path(r'^$', views.index, name='index'),
+    # ex: /polls/5/
+    re_path(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    re_path(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    re_path(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
